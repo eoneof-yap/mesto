6b4ca0205437ac01606a6761c37e907f8555598f
Nikolay Ivanov <eoneof@yandex.ru>
2022-02-26 19:53:12 +0300

    feat: add some vars

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,19 @@
-﻿let page = document.querySelector('.page');
+﻿// DOC
+let page = document.querySelector('.page');
 let popup = page.querySelector('.popup');
+
+// BUTTONS
 let editButton = page.querySelector('.button_action_edit');
 let addButton = page.querySelector('.button_action_add');
 let submitButton = page.querySelector('.button_action_submit');
 let closeButton = page.querySelector('.button_action_close');
 let likeButton = page.querySelector('.button_action_like');
+
+// INPUTS
+let currentUserName = page.querySelector('.profile__name').textContent;
+let currentUserInfo = page.querySelector('.profile__about').textContent;
+let newUserName = page.querySelector(/*  забрать из инпута */)
+let newUserInfo = page.querySelector(/*  забрать из инпута */)
 editButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.remove('popup_closed');
