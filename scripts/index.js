4673f7fa44d8196c62e860b03c28d75dbecfa6f0
Nikolay Ivanov <eoneof@yandex.ru>
2022-02-27 21:28:28 +0300

    fix: logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,39 @@
 ﻿// DOC
-let page = document.querySelector('.page');
-let popup = page.querySelector('.popup');
+const page = document.querySelector('.page');
+const popup = page.querySelector('.popup');
 
 // BUTTONS
-let editButton = page.querySelector('.button_action_edit');
-let addButton = page.querySelector('.button_action_add');
-let submitButton = page.querySelector('.button_action_submit');
-let closeButton = page.querySelector('.button_action_close');
-let likeButton = page.querySelector('.button_action_like');
+const editButton = page.querySelector('.button_action_edit');
+const addButton = page.querySelector('.button_action_add');
+const submitButton = page.querySelector('.button_action_submit');
+const closeButton = page.querySelector('.button_action_close');
+const likeButton = page.querySelector('.button_action_like');
 
 // INPUTS
 let currentUserName = page.querySelector('.profile__name');
 let currentUserInfo = page.querySelector('.profile__about');
 let newUserName = page.querySelector('input[name="user-name"]');
 let newUserInfo = page.querySelector('input[name="user-about"]');
+
+// LOGIC
 editButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   newUserName.value = currentUserName.textContent;
   newUserInfo.value = currentUserInfo.textContent;
-  popup.classList.remove('popup_closed');
+  popup.classList.add('popup_opened');
   newUserName.focus();
 });
 closeButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  popup.classList.add('popup_closed');
+  popup.classList.remove('popup_opened');
 });
-submitButton.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  if (newUserName.value | newUserInfo.value === '') {
-    popup.classList.add('popup_closed');
-  } else {
-    currentUserName.textContent = newUserName.value;
-    currentUserInfo.textContent = newUserInfo.value;
-    newUserName.value = '';
-    newUserInfo.value = '';
-    popup.classList.add('popup_closed');
-  }
-});
+submitButton.addEventListener('click', formSubmitHandler);
 
 // закрыть попап по по клавише Escape
 document.addEventListener('keyup', function (evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    popup.classList.add('popup_closed');
+    popup.classList.remove('popup_opened');
   }
 });
 
@@ -51,3 +42,16 @@ addButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   console.log(`Клик по ${addButton.classList}`);
 });
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  if (newUserName.value | (newUserInfo.value === '')) {
+    popup.classList.remove('popup_opened');
+  } else {
+    currentUserName.textContent = newUserName.value;
+    currentUserInfo.textContent = newUserInfo.value;
+    newUserName.value = '';
+    newUserInfo.value = '';
+    popup.classList.remove('popup_opened');
+  }
+}
