d4f24b22d7094eafd909cdf059adbf3ec9d2190d
Nikolay Ivanov <eoneof@yandex.ru>
2022-03-31 01:55:00 +0300

    feat: add basic validation logic

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,1 +1,30 @@
-﻿
+﻿function showError() {
+  profileNameErrorHint.classList.add('form__input-error-hint_active');
+}
+
+function hideError() {
+  profileNameErrorHint.classList.remove('form__input-error-hint_active');
+}
+
+function enableButton() {
+  editingFormSubmitButton.classList.remove('button_disabled');
+}
+
+function disableButton() {
+  editingFormSubmitButton.classList.add('button_disabled');
+}
+
+function checkValidity() {}
+
+function validationHandler(evt) {
+  if (!evt.target.checkValidity()) {
+    disableButton();
+    showError();
+  } else {
+    enableButton();
+    hideError();
+    console.log(evt.key);
+  }
+}
+
+newProfileNameInput.addEventListener('input', validationHandler);
