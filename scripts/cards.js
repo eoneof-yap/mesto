2233b160c6474eddf5e2bd4e86c90c5a3fea06e1
Nikolay Ivanov <eoneof@yandex.ru>
2022-04-07 16:26:53 +0300

    refactor: move data to external file

---
+++ scripts/cards.js
@@ -0,0 +1,27 @@
+﻿// DATA
+const initialCards = [
+  {
+    name: 'Пятигорск',
+    link: './images/pyatigorks.jpeg',
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elbrus.jpeg',
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombay-mountains.jpeg',
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombay-yak.jpeg',
+  },
+  {
+    name: 'Гора Машук',
+    link: './images/gora-mashuk.jpeg',
+  },
+  {
+    name: 'Кабардино-Балкария',
+    link: './images/kabardino-balkariya.jpeg',
+  },
+];
--- scripts/index.js
+++ scripts/index.js
@@ -30,34 +30,6 @@ const newProfileInfoInput = editingForm.elements['about-input'];
 const newPhotoNameInput = addingForm.elements['photo-name-input'];
 const newPhotoLinkInput = addingForm.elements['photo-link-input'];
 
-// DATA
-const initialCards = [
-  {
-    name: 'Пятигорск',
-    link: './images/pyatigorks.jpeg',
-  },
-  {
-    name: 'Гора Эльбрус',
-    link: './images/elbrus.jpeg',
-  },
-  {
-    name: 'Домбай',
-    link: './images/dombay-mountains.jpeg',
-  },
-  {
-    name: 'Домбай',
-    link: './images/dombay-yak.jpeg',
-  },
-  {
-    name: 'Гора Машук',
-    link: './images/gora-mashuk.jpeg',
-  },
-  {
-    name: 'Кабардино-Балкария',
-    link: './images/kabardino-balkariya.jpeg',
-  },
-];
-
 // FUNCTIONS
 function preventDefaultBehavior(evt) {
   evt.preventDefault();
