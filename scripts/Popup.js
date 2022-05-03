207621966aca1d05a7de2973be32460762af1672
Nikolay Ivanov <eoneof@yandex.ru>
2022-05-04 01:05:54 +0300

    create dummy classes

---
+++ scripts/Popup.js
@@ -0,0 +1,72 @@
+﻿class Popup {
+  constructor(popupSelector) {
+    this.popupSelector = popupSelector;
+  }
+
+  open() {
+    // openPopup
+    this.popupSelector.classList.add('popup_opened');
+  }
+
+  close() {
+    // closePopup
+    this.popupSelector.classList.remove('popup_opened');
+  }
+
+  setEventListeners() {
+    // popupCloseButton
+  }
+
+  _handleEscClose() {
+    // escape > close()
+  }
+}
+
+class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+  }
+
+  open() {
+    // addImage
+  }
+}
+
+class PopupWithForm extends Popup {
+  constructor(popupSelector /*, callback */) {
+    super(popupSelector);
+    // this.callback = callback
+  }
+
+  close() {
+    // closePopup
+    // resetForm
+  }
+
+  setEventListeners() {
+    // submitForm
+  }
+
+  _getInputValues() {
+    // inputs data
+  }
+}
+
+// ---------------------------------------------------------------------------
+// TEST
+const editingForm = document.forms['form-edit'];
+const page = document.querySelector('.page');
+const editProfilePopup = page.querySelector('.popup_type_edit');
+
+const popup = new Popup('this is a Popup yeah!');
+// const popup2 = new PopupWithImage('this is a PopupWithImage yeah!');
+popup.open();
+
+function editingFormSubmitHandler(evt) {
+  preventDefaultBehavior(evt);
+  currentProfileName.textContent = newProfileNameInput.value;
+  currentProfileInfo.textContent = newProfileInfoInput.value;
+  closePopup(editProfilePopup);
+}
+
+editingForm.addEventListener('submit', editingFormSubmitHandler);
---
+++ scripts/Section.js
@@ -0,0 +1,13 @@
+﻿class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this.items = items;
+    this.renderer = renderer;
+    this.containerSelector = containerSelector;
+  }
+
+  renderCard() {
+    this.renderer; // index.js -> *Section.js* -> Card.js
+  }
+
+  addItem() {}
+}
